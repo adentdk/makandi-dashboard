4a65cbbe19caa3ce13bf8cb6a067c64c7e452ffe
Aden Trisna Daud Kurnia <adentrisnadaudkurnia@gmail.com>
2021-01-03 01:51:41 +0700

    integrating api

--- src/components/NavNavigationBar.vue
+++ src/components/NavNavigationBar.vue
@@ -5,12 +5,19 @@
       :mini-variant="mini"
       app
     >
-      <v-list-item class="px-2">
+      <v-list-item :class="mini ? 'py-1 px-2' : ''">
         <v-list-item-avatar @click="miniToggle">
           <v-img :src="logo"></v-img>
         </v-list-item-avatar>
 
-        <v-list-item-title>MakanDI</v-list-item-title>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            MakanDI
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Web Admin
+          </v-list-item-subtitle>
+        </v-list-item-content>
 
         <v-btn
           icon
@@ -20,9 +27,70 @@
         </v-btn>
       </v-list-item>
 
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+        shaped
+      >
+        <div
+          v-for="access in accesses"
+          :key="access.name"
+        >
+          <v-list-group
+            v-if="access.hasSub"
+            link
+            :to="access.path"
+            no-action
+            :prepend-icon="access.icon"
+            :append-icon="access.hasSub ? undefined : ''"
+          >
+            <template v-slot:activator>
+              <v-list-item-title>{{access.name}}</v-list-item-title>
+            </template>
+
+            <v-list-item
+              v-for="subAccess in access.sub"
+              :key="subAccess.name"
+              link
+              :to="`${access.path}/${subAccess.path}`"
+            >
+              <v-list-item-content>
+                <v-list-item-title>{{ subAccess.name }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+
+          <v-list-item
+            v-else
+            link
+            :to="access.path"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ access.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ access.name }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </div>
+      </v-list>
+
     </v-navigation-drawer>
 
     <v-app-bar color="white" elevation="2" app>
+      <div>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="font-weight-bold">{{companyProfile.name}}</v-list-item-title>
+            <v-list-item-subtitle>
+              &nbsp;
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </div>
       <v-spacer></v-spacer>
       <v-menu
         v-model="menu"
@@ -37,25 +105,22 @@
             v-bind="attrs"
             v-on="on"
           >
-            <v-icon>mdi-dots-vertical</v-icon>
+             <v-avatar width="35" height="35">
+                <img
+                  :src="profile.image_url"
+                  :alt="profile.name"
+                >
+              </v-avatar>
           </v-btn>
         </template>
 
         <v-card>
           <v-list>
             <v-list-item>
-              <v-list-item-avatar>
-                <img
-                  src="https://cdn.vuetifyjs.com/images/john.jpg"
-                  alt="John"
-                >
-              </v-list-item-avatar>
-
               <v-list-item-content>
-                <v-list-item-title>Aden Trisna Daud Kurnia</v-list-item-title>
-                <v-list-item-subtitle>Super Admin</v-list-item-subtitle>
+                <v-list-item-title>{{profile.name}}</v-list-item-title>
+                <v-list-item-subtitle>{{profile.role.name}}</v-list-item-subtitle>
               </v-list-item-content>
-
               <v-list-item-action>
                 <v-btn
                   class="primary--text"
@@ -64,6 +129,7 @@
                   <v-icon>mdi-account-settings</v-icon>
                 </v-btn>
               </v-list-item-action>
+
             </v-list-item>
           </v-list>
           <v-divider></v-divider>
@@ -93,8 +159,11 @@ export default {
     return {
       mini: false,
       menu: false,
-      logo: 'https://firebasestorage.googleapis.com/v0/b/makan-di.appspot.com/o/public%2Fsite%2Flogo200x200white.png?alt=media&token=b167ef3b-14df-43c9-a59c-84e64b19c403'
+      logo: 'https://firebasestorage.googleapis.com/v0/b/makan-di.appspot.com/o/public%2Fsite%2Flogo50x50white.png?alt=media&token=7ccd6b5e-c1b5-4240-89d4-29aa1e48ec6d'
     }
+  },
+  computed: {
+    ...auth.mapState(['profile', 'companyProfile', 'accesses'])
   },
   methods: {
     ...auth.mapMutations(['setToken']),
